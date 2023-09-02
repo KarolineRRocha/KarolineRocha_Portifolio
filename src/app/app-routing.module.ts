d568b6a92e5628679242f340a58a12c5a5430dba
KarolineRRocha <emaildakarolineribeiro@gmail.com>
2023-09-02 16:01:39 +0100

    Start project page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { ProjectPageComponent } from './project-page/project-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'about', component: AboutPageComponent },
-  { path: 'contact', component: ContactPageComponent }
+  { path: 'contact', component: ContactPageComponent },
+  { path: 'projects', component: ProjectPageComponent }
 ];
 
 @NgModule({
---
+++ src/app/project-page/details-project-page/details-project-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-project-page',
+  templateUrl: './details-project-page.component.html',
+  styleUrls: ['./details-project-page.component.scss']
+})
+export class DetailsProjectPageComponent {
+
+}
